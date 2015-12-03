1fd992169d3affeda1ef5395695fcf8f04642704
Márcio S. Souza <marciodrummer@hotmail.com>
2015-12-03 09:02:54 -0200

    Inicial commit

---
+++ ClientSearch/Form1.Designer.vb
@@ -0,0 +1,258 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.mnuMain = New System.Windows.Forms.MenuStrip()
+        Me.ProjectToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewClientToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tbcMain = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.tbcClient = New System.Windows.Forms.TabControl()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.dgvNewClient = New System.Windows.Forms.DataGridView()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.mnuMain.SuspendLayout()
+        Me.tbcMain.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.tbcClient.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        CType(Me.dgvNewClient, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage4.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'mnuMain
+        '
+        Me.mnuMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ProjectToolStripMenuItem})
+        Me.mnuMain.Location = New System.Drawing.Point(0, 0)
+        Me.mnuMain.Name = "mnuMain"
+        Me.mnuMain.Size = New System.Drawing.Size(834, 24)
+        Me.mnuMain.TabIndex = 1
+        Me.mnuMain.Text = "MenuStrip1"
+        '
+        'ProjectToolStripMenuItem
+        '
+        Me.ProjectToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewClientToolStripMenuItem})
+        Me.ProjectToolStripMenuItem.Name = "ProjectToolStripMenuItem"
+        Me.ProjectToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.ProjectToolStripMenuItem.Text = "Project"
+        '
+        'NewClientToolStripMenuItem
+        '
+        Me.NewClientToolStripMenuItem.CheckOnClick = True
+        Me.NewClientToolStripMenuItem.Name = "NewClientToolStripMenuItem"
+        Me.NewClientToolStripMenuItem.Size = New System.Drawing.Size(132, 22)
+        Me.NewClientToolStripMenuItem.Text = "New Client"
+        '
+        'tbcMain
+        '
+        Me.tbcMain.Controls.Add(Me.TabPage1)
+        Me.tbcMain.Controls.Add(Me.TabPage2)
+        Me.tbcMain.Location = New System.Drawing.Point(9, 33)
+        Me.tbcMain.Name = "tbcMain"
+        Me.tbcMain.SelectedIndex = 0
+        Me.tbcMain.Size = New System.Drawing.Size(813, 444)
+        Me.tbcMain.TabIndex = 3
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.FlowLayoutPanel1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(805, 418)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Client"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.tbcClient)
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(814, 423)
+        Me.FlowLayoutPanel1.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.TabControl1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(805, 418)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "List"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'tbcClient
+        '
+        Me.tbcClient.Controls.Add(Me.TabPage3)
+        Me.tbcClient.Location = New System.Drawing.Point(3, 3)
+        Me.tbcClient.Name = "tbcClient"
+        Me.tbcClient.SelectedIndex = 0
+        Me.tbcClient.Size = New System.Drawing.Size(796, 409)
+        Me.tbcClient.TabIndex = 3
+        Me.tbcClient.Visible = False
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.dgvNewClient)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(788, 383)
+        Me.TabPage3.TabIndex = 0
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'dgvNewClient
+        '
+        Me.dgvNewClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvNewClient.Location = New System.Drawing.Point(3, 3)
+        Me.dgvNewClient.Name = "dgvNewClient"
+        Me.dgvNewClient.ReadOnly = True
+        Me.dgvNewClient.Size = New System.Drawing.Size(779, 374)
+        Me.dgvNewClient.TabIndex = 1
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Location = New System.Drawing.Point(6, 6)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(796, 406)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage4
+        '
+        Me.TabPage4.Controls.Add(Me.DataGridView1)
+        Me.TabPage4.Controls.Add(Me.Panel1)
+        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage4.Size = New System.Drawing.Size(788, 380)
+        Me.TabPage4.TabIndex = 0
+        Me.TabPage4.Text = "Search"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 120)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(776, 254)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(4, 6)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(795, 108)
+        Me.Panel1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(33, 35)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(133, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Search"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ID"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(33, 9)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(133, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(834, 489)
+        Me.Controls.Add(Me.tbcMain)
+        Me.Controls.Add(Me.mnuMain)
+        Me.MainMenuStrip = Me.mnuMain
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.mnuMain.ResumeLayout(False)
+        Me.mnuMain.PerformLayout()
+        Me.tbcMain.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.tbcClient.ResumeLayout(False)
+        Me.TabPage3.ResumeLayout(False)
+        CType(Me.dgvNewClient, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage4.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents mnuMain As MenuStrip
+    Friend WithEvents ProjectToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewClientToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents tbcMain As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents tbcClient As TabControl
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents dgvNewClient As DataGridView
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage4 As TabPage
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ ClientSearch/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Private Sub NewClientToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewClientToolStripMenuItem.Click
+
+        If sender.ToString() = "New Client" Then
+            Dim strPath As String
+
+            Call LoadFile()
+        End If
+
+
+    End Sub
+
+    Private Sub LoadFile()
+        Dim myStream As IO.Stream = Nothing
+        Dim openFileDialog1 As New OpenFileDialog()
+        Dim strPath As String
+        Dim strClient As String
+        Dim MyConnection As System.Data.OleDb.OleDbConnection
+        Dim DtSet As System.Data.DataSet
+        Dim MyCommand As System.Data.OleDb.OleDbDataAdapter
+
+
+        openFileDialog1.InitialDirectory = "c:\"
+        openFileDialog1.Filter = "xls files (*.xls)|*.xls"
+        openFileDialog1.FilterIndex = 2
+        openFileDialog1.RestoreDirectory = True
+
+        strClient = InputBox("Enter Client Name:", "Client")
+
+        If Trim(strClient) <> "" Then
+
+            If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                Try
+                    If openFileDialog1.CheckPathExists = True Then
+                        strPath = openFileDialog1.FileName
+                        MyConnection = New System.Data.OleDb.OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;Data Source='" & strPath & "';Extended Properties=Excel 8.0;")
+                        MyCommand = New System.Data.OleDb.OleDbDataAdapter("select * from [Plan1$]", MyConnection)
+                        MyCommand.TableMappings.Add("Table", "Net-informations.com")
+                        DtSet = New System.Data.DataSet
+                        MyCommand.Fill(DtSet)
+                        dgvNewClient.DataSource = DtSet.Tables(0)
+                        MyConnection.Close()
+                    End If
+                Catch Ex As Exception
+                    MessageBox.Show("Cannot read file from disk. Original error: " & Ex.Message)
+                Finally
+                    ' Check this again, since we need to make sure we didn't throw an exception on open.
+                    If (myStream IsNot Nothing) Then
+                        myStream.Close()
+                    End If
+                End Try
+            End If
+        End If
+    End Sub
+
+
+End Class
